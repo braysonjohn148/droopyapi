264604e5c341691257a27155d9d1f85ab1f6dcd6
braysonjohn148 <41685357+braysonjohn148@users.noreply.github.com>
2019-11-26 00:59:41 +0300

    Add files via upload

---
+++ droopy/Operations/chats/homepageChecknewmsg.php
@@ -0,0 +1,34 @@
+<?php
+// include database and object files
+include_once '../../DatabaseConnection/databaseconnection.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+
+$data = json_decode(file_get_contents("php://input"));
+
+    if($data != null){
+
+        $userID = $data -> userID;
+
+    }else{
+        echo "error";
+    }
+
+   $sql ="SELECT * FROM inbox WHERE idTo = '$userID' AND isread = 0";
+   $statement = $db->prepare($sql);  $statement->execute();
+
+//    while ($row = $statement->fetch()) {
+//     $response["messageID"] = $row['messageID'];
+    
+//     }
+
+   $total_rows = $statement->rowCount();
+    
+
+    $response["result"] = "success";
+    $response["number"] = $total_rows;
+    echo json_encode($response);
+
+?>
