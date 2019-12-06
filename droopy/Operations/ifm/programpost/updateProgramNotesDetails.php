6d0c8b26af2b5652c1a5223b7de84594dcd4a40d
braysonjohn148 <41685357+braysonjohn148@users.noreply.github.com>
2019-12-07 00:13:39 +0300

    Create updateProgramNotesDetails.php

---
+++ droopy/Operations/ifm/programpost/updateProgramNotesDetails.php
@@ -0,0 +1,30 @@
+<?php
+// include database and object files
+include_once '../../DatabaseConnection/databaseconnection.php';
+
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+
+$data = json_decode(file_get_contents("php://input"));
+
+    if($data != null){
+
+        $id = $data -> id;
+        $subjectname = $data -> subjectname;
+        $semister = $data -> semister;
+        $year = $data -> year;
+
+}else{
+    echo "error";
+}
+
+$sql = "UPDATE programnotesdetails SET subjectname = '$subjectname'  semister = '$semister' year = '$year' WHERE id = '$id'";
+$statement = $db->prepare($sql);  $statement->execute();
+
+
+$response["result"] = "success";
+$response["message"] = "Subject updated succesfully.";
+echo json_encode($response);
+?>
