08ab8bca1eb6ec49aa0061ea120704e97addc5b7
braysonjohn148 <41685357+braysonjohn148@users.noreply.github.com>
2019-12-10 22:09:13 +0300

    Update deleteNotes.php

--- droopy/Operations/ifm/notes/deleteNotes.php
+++ droopy/Operations/ifm/notes/deleteNotes.php
@@ -9,14 +9,12 @@ $data = json_decode(file_get_contents("php://input"));
     if($data != null){
         $notesID = $data -> notesID;
      
-            $sql = "UPDATE notes SET notesID = $notesID+' - Deleted' WHERE notesID = '$notesID'";
-            $statement = $db->prepare($sql);   
-         
-       $statement = $db->prepare($sql);  $statement->execute();
-     
-            $response["result"] = "success";
-            $response["message"] = "Posted Successfully !";
-            echo json_encode($response);
+
+      $sql = "UPDATE notes SET notesID = '$notesID - Deleted' WHERE notesID = '$notesID'";
+      $statement = $db->prepare($sql);  $statement->execute();
+      $response["result"] = "success";
+      $response["message"] = "Notes Deleted succesfully.";
+      echo json_encode($response);
        
     }else{
     echo "error";
